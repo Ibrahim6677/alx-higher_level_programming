807b7f4d2f0fc2c937c341ff724d33a562a8591c
ibrahim <ibrahimsaad6677@gmail.com>
2024-01-05 17:40:40 +0200

    finshed

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,4 +1,4 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 """
 Defines a class Rectangle
 """
@@ -23,7 +23,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self,__width = value
+        self.__width = value
 
     @property
     def height(self):
