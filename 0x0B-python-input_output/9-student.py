8804ebc2fba70c83da26ddfc2748d8e976439322
ibrahim <ibrahimsaad6677@gmail.com>
2024-01-10 02:21:35 +0200

    input/output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""contains the class "Student"""
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initialies the student"""
+        self.first_name = first_name
+        self.lasr_name = lasr_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary representation of Student ubstance"""
+        return self.__dict__
