5a42af89a2cad889574ba4dcffe8d5d30eba2a23
ibrahim <ibrahimsaad6677@gmail.com>
2024-03-18 19:14:37 +0200

    python

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,33 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Prepare SQL query to fetch data from the states table
+    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+    try:
+        # Execute the SQL command
+        cursor.execute(sql_query, (state_name,))
+        # Fetch all the rows in a list of tuples
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except MySQLdb.Error as e:
+        print("Error: unable to fetch data")
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+
+    # Disconnect from server
     db.close()
+
