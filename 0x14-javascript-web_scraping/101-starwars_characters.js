936bc2b391a48b2483abe461c588cc9606767e23
ibrahim <ibrahimsaad6677@gmail.com>
2024-04-23 17:07:34 -0700

    web

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,42 +3,46 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-//  the 'request' module to perform an HTTP GET request to the Star Wars API URL.
-request(apiUrl, function (error, response, body) {
-  // Check if there was no error during the HTTP request
-  if (!error && response.statusCode === 200) {
-    // Parse the JSON response bod
-    const movieData = JSON.parse(body);
-    // create an array of promises that fetch the data for each individual character.
-    const characterPromises = movieData.characters.map((characterUrl) => {
-      return new Promise((resolve, reject) => {
-        // Use another 'request' to fetch the data for the individual character.
-        request(characterUrl, function (charError, charResponse, charBody) {
-          // Check if there was no error during the HTTP request
-          if (!charError && charResponse.statusCode === 200) {
-            // Parse the JSON response body
-            const characterData = JSON.parse(charBody);
-            // Resolve the promise with the name of the character.
-            resolve(characterData.name);
-          } else {
-            // reject the promise with the error message if  there was an error during the HTTP request
-            reject(new Error(`Error fetching character data: ${charError}`));
-          }
-        });
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  const characterUrls = movie.characters;
+
+  const fetchCharacter = (url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        const character = JSON.parse(body);
+        resolve(character.name);
       });
     });
+  };
 
-    Promise.all(characterPromises)
-      .then((characterNames) => {
-        console.log(characterNames.join('\n'));
-      })
-      .catch((error) => {
-        console.error(error.message);
-      });
-  } else {
-    console.error('Error fetching movie data:', error);
-  }
+  const fetchAllCharacters = async () => {
+    const characterNames = [];
+    for (const url of characterUrls) {
+      try {
+        const name = await fetchCharacter(url);
+        characterNames.push(name);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    return characterNames;
+  };
+
+  fetchAllCharacters()
+    .then(names => {
+      names.forEach(name => console.log(name));
+    })
+    .catch(err => console.error(err));
 });
